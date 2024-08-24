e075e90b788381a317a3155cf224bf26103e2378
amr <amr.mosaad@vodafone.com.eg>
2024-08-24 15:34:50 +0300

    add dead letter queue handling

--- src/main/java/com/SpringCloud/KafkaStream/Service/ConService.java
+++ src/main/java/com/SpringCloud/KafkaStream/Service/ConService.java
@@ -3,13 +3,19 @@ package com.SpringCloud.KafkaStream.Service;
 import java.util.function.Consumer;
 
 import com.SpringCloud.KafkaStream.Domain.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.DltHandler;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConService {
-
 
     //this::handleEvent is a method reference that refers to the handleEvent method. This means that when the Consumer receives a message, it will pass that message to the handleEvent method for processing.
     @Bean
@@ -17,7 +23,25 @@ public class ConService {
         return this::handleEvent;
     }
 
+
     public void handleEvent(Message<Order> message) {
-        System.out.println("in consumer " + message.getPayload().toString());
+
+
+        try {
+
+            // example that we can't process any order with id = 1
+            if(message.getPayload().getOrder_id().equals("1")){
+                throw new RuntimeException("Failed to process order with ID 1");
+            }
+        }
+        catch (Exception e){
+            // If an error occurs, send the message to the DLQ
+            System.out.println("consumer failed to process the message: " + message);
+            throw e;// to trigger DLQ
+        }
+
     }
+
+
+
 }
